a791733d5dfc7640c39bb3936637840869dc07ff
afbiggs <112245251+afbiggs@users.noreply.github.com>
2023-03-12 22:49:48 -0400

    added proper file strcture

--- models/thought.js
+++ models/thought.js
@@ -1,45 +1,4 @@
-// const { Schema, model } = require('mongoose');
-// const dateFormat = require('../utils/dateFormat');
-// const reactionSchema = require('./Reaction');
 
-// // thought schema
-// const thoughtSchema = new Schema(
-//     {
-//         thoughtText: {
-//             type: String,
-//             required: true,
-//             maxlength: 280
-//         },
-//         createdAt: {
-//             type: Date,
-//             default: Date.now,
-//             get: (createdAtVal) => dateFormat(createdAtVal)
-//         },
-//         username: {
-//             type: String,
-//             required: true
-//         },
-//         // tying reactions to thought
-//         reactions: [reactionSchema]
-//     },
-//     {
-//         toJSON: {
-//             virtuals: true,
-//             getters: true
-//         },
-//         id: false
-//     }
-// );
-
-// // gets total friend count 
-// thoughtSchema.virtual('reactionCount').get(function () {
-//     return this.reactions.length;
-// });
-
-// // create the User model using the UserSchema
-// const Thought = model('Thought', thoughtSchema);
-
-// module.exports = { Thought };
 
 const { Schema, model, Types } = require('mongoose')
 const moment = require('moment')
@@ -105,36 +64,6 @@ const ThoughtSchema = new Schema(
   },
 )
 
-// const ReactionSchema = new Schema(
-//   {
-//     reactionId: {
-//       type: Schema.Types.ObjectId,
-//       default: () => Types.ObjectId(),
-//     },
-//     reactionBody: {
-//       type: String,
-//       required: true,
-//       trim: true,
-//       minlenght: 1,
-//       maxlenght: 280,
-//     },
-//     username: {
-//       type: String,
-//       required: true,
-//     },
-//     createdAt: {
-//       type: Date,
-//       default: Date.now,
-//       get: (createdAtVal) =>
-//         moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a'),
-//     },
-//   },
-//   {
-//     toJSON: {
-//       getters: true,
-//     },
-//   },
-// )
 const Thought = model('Thought', ThoughtSchema)
 
 ThoughtSchema.virtual('reactionCount').get(function () {
